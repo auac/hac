9a5678fd1ea477764263f52dd24ea9329a352d61
auac <36478177+auac@users.noreply.github.com>
2018-03-21 03:33:09 +0800

    Notification automation update (#27)
    * update channel slack py
    
    * update python version
    
    * chg travis script
    
    * chg travis script#2
    
    * chg travis script#3
    
    * chg travis script#4
    
    * chg travis script#5
    
    * chg travis script#6
    
    * chg travis script#7
    
    * chg travis script#8
    
    * chg travis script#9
    
    * chg travis script#10

--- python_scripts/notification.py
+++ python_scripts/notification.py
@@ -1,15 +1,71 @@
 # Get time and settings
-time                = hass.states.get('sensor.time').state
+now                 = datetime.datetime.now()
+t                   = hass.states.get('sensor.time').state
+timestamp           = "{}_{}_{}_{}_{}_{}_{}".format(
+                      now.year, now.month, now.day, now.hour,
+                      now.minute, now.second, now.microsecond)
 sending             = 'on'
+send_image          = 0
+count               = -1
+aa                  = hass.states.get('input_boolean.presence_a').state
 
 # Get script variables
-target      = data.get('target') or 'notification'
+target      = data.get('target') or '#info'
 msg         = ''
 color       = data.get('color') or 'good'
 title       = data.get('message') or 'HA automation' 
-text        = data.get('text') or (' @ ' + time)
+text        = data.get('text') or (' @ ' + t)
+entity_id   = data.get('entity_id') or 'ENTITY_IDError'
+path        = data.get('path') or 'PATHError'
+device_class= data.get('device_class') 
 
-# Call service
-if sending == 'on' :
-    data = { "target" : target, "message" : msg , "data" : { "attachments" : [ { "color" : color , "title" : title , "text" : text } ] } }
+# check if image sending is required and 
+# define the camera to take the image from
+
+entitys = ['binary_sensor.main_door_sensor_sensor', 'binary_sensor.multisensor1_sensor']
+cameras = ['camera.living_room_camera', 'camera.living_room_camera']
+
+for entity in entitys :
+    count = count + 1
+    if entity == entity_id :
+        send_image = send_image + 1
+        camera_id  = cameras[count]
+
+# create image file if image file required
+    
+if send_image == 1 :
+    if entity_id == 'ENTITY_IDError' or path == 'PATHError' :
+        text = text + ' ' + entity_id + ' ' + path
+    else :
+        filename = path + '/' + timestamp + entity_id + '.jpg' 			# filename key for service call               
+        data = { "entity_id" : camera_id , "filename" : filename }
+        hass.services.call('camera', 'snapshot', data )
+#        while not os.path.exists(filename):
+        time.sleep(5)
+
+# Call service and send to target channel if specified
+if target != '#info' :
+    if send_image == 1 :
+        data = { "target" : target , "message" : title , "title" : text , "data" : { "file" : { "path" : filename } } }
+    else :
+        data = { "target" : target , "message" : msg , "data" : { "attachments" : [ { "color" : color , "title" : title , "text" : text } ] } }
     hass.services.call('notify', 'slack', data)
+
+# Call service and send to notification all the time
+if sending == 'on' :
+    target = '#info'
+    if send_image == 1 :
+        data = { "target" : target , "message" : title , "title" : text , "data" : { "file" : { "path" : filename } } }
+    else :
+        data = { "target" : target , "message" : msg , "data" : { "attachments" : [ { "color" : color , "title" : title , "text" : text } ] } }
+    hass.services.call('notify', 'slack', data)
+
+# Call service and send to warning channel (or if aa not home):
+if (device_class == 'connectivity' or device_class == 'door' or device_class == 'window' or device_class == 'opening' or device_class == 'occupancy') or ( aa == 'off') or ( entity_id == 'binary_sensor.multisensor1_sensor'):
+    target = '#warn'
+    if send_image == 1 :
+        data = { "target" : target , "message" : title , "title" : text , "data" : { "file" : { "path" : filename } } }
+    else :
+        data = { "target" : target , "message" : msg , "data" : { "attachments" : [ { "color" : color , "title" : title , "text" : text } ] } }
+    hass.services.call('notify', 'slack', data)
+    
